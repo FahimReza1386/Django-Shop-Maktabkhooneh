e3b4a0ae3a713ab43a3b8a3676b30e5de85ea3a2
FahimReza <FahimReza202@outlook.com>
2025-01-01 16:42:53 +0330

    Save The Change of filters in product-grid page

--- core/shop/views.py
+++ core/shop/views.py
@@ -4,13 +4,32 @@ from django.views.generic import (
     ListView,
     DetailView,
 )
+from django.core.exceptions import FieldError
 from .models import ProductModel, ProductStatusType, ProductCategoryModel
+from django.utils.translation import gettext_lazy as _
+from django.contrib import messages
 
 # Create your views here.
 class ShopProductGridView(ListView):
     template_name = "Shop/product-grid.html"
     model = ProductModel
     paginate_by = 9
+
+
+    def get_paginate_by(self, queryset):
+        
+        valid_page_sizes = {12, 15, 18, 21, 24}
+
+        page_size_param = self.request.GET.get('page_size')
+
+        try:
+            page_size = int(page_size_param)
+            if page_size > 0 or page_size in valid_page_sizes:
+                return page_size
+            else:
+                return self.paginate_by
+        except:
+            return self.paginate_by
 
     def get_queryset(self):
         queryset =ProductModel.objects.filter(status=ProductStatusType.publish.value)
@@ -25,7 +44,11 @@ class ShopProductGridView(ListView):
 
         if category_id := self.request.GET.get("category_id"):
             queryset = queryset.filter(category__id=category_id)
-
+        if order_by := self.request.GET.get("order_by"):
+            try:
+                queryset = queryset.order_by(order_by)
+            except FieldError:
+                messages.error(self.request , ("خطا در وارد کردن فیلد"))
 
         return queryset
 
