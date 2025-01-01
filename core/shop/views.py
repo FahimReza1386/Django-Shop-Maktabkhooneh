9a15fe27328c2e45aecad34d5d072722ef7ab94e
FahimReza <FahimReza202@outlook.com>
2025-01-01 12:30:16 +0330

    Delete Picture and Create The Filter in Product-grid url

--- core/shop/views.py
+++ core/shop/views.py
@@ -4,18 +4,35 @@ from django.views.generic import (
     ListView,
     DetailView,
 )
-from .models import ProductModel, ProductStatusType
+from .models import ProductModel, ProductStatusType, ProductCategoryModel
 
 # Create your views here.
 class ShopProductGridView(ListView):
     template_name = "Shop/product-grid.html"
     model = ProductModel
-    querysets=ProductModel.objects.filter(status=ProductStatusType.publish.value)
     paginate_by = 9
+
+    def get_queryset(self):
+        queryset =ProductModel.objects.filter(status=ProductStatusType.publish.value)
+        if search_q := self.request.GET.get("q"):
+            queryset = queryset.filter(title__icontains=search_q)
+
+        if min_price := self.request.GET.get("min_price"):
+            queryset = queryset.filter(price__gte=min_price)
+
+        if max_price := self.request.GET.get("max_price"):
+            queryset = queryset.filter(price__lte=max_price)
+
+        if category_id := self.request.GET.get("category_id"):
+            queryset = queryset.filter(category__id=category_id)
+
+
+        return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['total_items'] = self.get_queryset().count()
+        context['categories'] = ProductCategoryModel.objects.all()
         return context
 
 
