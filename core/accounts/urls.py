eb0da83da80ca54a541a89734541113dc28657cd
FahimReza <FahimReza202@outlook.com>
2025-01-04 21:54:57 +0330

    Craete The Reset Password in accounts app and save the changed of all pages

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = "accounts"
 urlpatterns= [
     path("login/", views.LoginView.as_view() , name="login"),
-    path("logout/", views.LogoutView.as_view() , name="logout")
-    # path("register/", views.RegisterView.as_view() , name="register")
+    path("logout/", views.LogoutView.as_view() , name="logout"),
+    path("reset_password/", views.ResetPasswordView.as_view() , name="reset_password"),
+    path('password-reset-confirm/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html',success_url='/accounts/password-reset-complete/'),
+        name='password_reset_confirm'),
+
+    path('password-reset-complete/',views.CustomPasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
 ]
