699c934b0a0fcdb67987cf25c4d4f670007a2d75
FahimReza <FahimReza202@outlook.com>
2025-01-04 23:49:58 +0330

    Create The Conatct Model

--- core/website/admin.py
+++ core/website/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from website.models import Contact
 # Register your models here.
+
+class CustomContact(admin.ModelAdmin):
+    model=Contact
+    list_display=("email", "subject", "is_checked",)
+    list_filter=("is_checked",)
+    searching_fields=("first_name", "last_name", "subject", "phone_number", "description",)
+    ordering=("is_checked",)
+
+
+
+admin.site.register(Contact, CustomContact)
