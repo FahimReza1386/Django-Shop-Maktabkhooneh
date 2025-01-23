b33477204581c5f1007ab6177ad7571eda76c1aa
FahimReza <FahimReza202@outlook.com>
2025-01-23 18:06:37 +0330

    Adding The Profile Image Edit page and url and view in Project

--- core/dashboard/admin/views.py
+++ core/dashboard/admin/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth import views as auth_views
 from django.urls import reverse_lazy
 from dashboard.admin.forms import AdminPasswordChangeForm, AdminProfileEditForm
 from accounts.models import Profile
+from django.contrib import messages
 # Create your views here.
 
 class AdminDashBoardHomeView(LoginRequiredMixin, HasAdminAccessPermission, TemplateView):
@@ -28,3 +29,19 @@ class AdminProfileEditView(LoginRequiredMixin, HasAdminAccessPermission,SuccessM
     def get_object(self, queryset = None):
 
         return Profile.objects.get(user=self.request.user)
+    
+
+class AdminProfileImageEditView(LoginRequiredMixin, HasAdminAccessPermission, SuccessMessageMixin, UpdateView):
+    model = Profile
+    fields = ["image"]
+    http_method_names = ["post"]
+    success_url = reverse_lazy("dashboard:dash_admin:profile-edit")
+    success_message = "بروزرسانی تصویر پروفایل با موفقیت انجام شد ."
+
+    def get_object(self, queryset = None):
+
+        return Profile.objects.get(user=self.request.user)
+    
+    def form_invalid(self, form):
+        messages.error(self.request,"ارسال تصویر با مشکل مواجه شد . لطفا محددا بررسی و تلاش نمایید . ")
+        return redirect(self.success_url)
