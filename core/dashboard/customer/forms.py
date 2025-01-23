0fd5808b6681f836eaf1b4ba16d80bf0dcbf39c2
FahimReza <FahimReza202@outlook.com>
2025-01-23 19:59:07 +0330

    Create The Dahboard for customer and save the changed all pages

---
+++ core/dashboard/customer/forms.py
@@ -0,0 +1,17 @@
+from django.contrib.auth import forms as auth_forms
+from django import forms
+from django.utils.translation import gettext_lazy as _
+from accounts.models import Profile
+
+class CustomerPasswordChangeForm(auth_forms.PasswordChangeForm):
+    error_messages = {
+        "password_mismatch": _("پسورد جدید و تکرار آن باید یکسان باشند ."),
+        "password_incorrect": _(
+            "پسورد فعلی شما اشتباه است اگر به یاد نمی اورید میتواند برای فراموش رمز عبور اقدام کنید."
+        ),
+    }
+
+class CustomerProfileEditForm(forms.ModelForm):
+    class Meta:
+        model= Profile
+        fields= ["first_name","last_name","phone_number","gender"]
--- core/dashboard/customer/urls.py
+++ core/dashboard/customer/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
 
+
 app_name = "dash_customer"
 
 urlpatterns = [
     path("home/", views.CustomerDashBoardHomeView.as_view(), name="home"),
+    path("security/edit/", views.CustomerSecurityEditView.as_view(), name="security-edit"),
+    path("profile/edit/", views.CustomerProfileEditView.as_view(), name="profile-edit"),
+    path("profile/image/edit/", views.CustomerProfileImageEditView.as_view(), name="profile-image-edit"),
 ]
