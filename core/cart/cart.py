476bb826fdf18ba67a24661fa96c5619b16c06aa
FahimReza <FahimReza202@outlook.com>
2025-01-29 06:29:36 +0330

    Save The Changed of cart and save the cart to db

--- core/cart/cart.py
+++ core/cart/cart.py
@@ -1,4 +1,6 @@
 from shop.models import ProductModel, ProductStatusType
+from cart.models import CartItemModel, CartModel
+
 class CartSession:
     total_payment_price = 0
     def __init__(self, session):
@@ -77,10 +79,39 @@ class CartSession:
             total_price = int(item["quantity"]) * product_obj.get_price_after_sale()
             item["total_price"] = total_price
             self.total_payment_price += total_price
-
         
         return cart_items
+
+    def sync_cart_items_from_db(self, user):
+        cart,created = CartModel.objects.get_or_create(user=user)
+        cart_items = CartItemModel.objects.filter(cart=cart)
+        for cart_item in cart_items:
+            for item in self._cart["items"]:
+                if str(cart_item.product.id) == item["product_id"]:
+                    cart_item.quantity = item["quantity"]
+                    cart_item.save()    
+                    break
+            else:
+                new_item = {"product_id":str(cart_item.product.id),"quantity" : cart_item.quantity}
+                self._cart["items"].append(new_item)
+        self.merge_session_cart_in_db(user=user)
+        self.save()
+
+    def merge_session_cart_in_db(self, user):
+        cart,created = CartModel.objects.get_or_create(user=user)
+
+        for item in self._cart["items"]:
+            product_obj = ProductModel.objects.get(id=item["product_id"],status=ProductStatusType.publish.value)
+            
+            cart_items,created = CartItemModel.objects.get_or_create(cart=cart, product=product_obj)
+            cart_items.quantity = item["quantity"]
+            cart_items.save()
+
+        session_product_ids = [item["product_id"] for item in self._cart["items"]]
+        CartItemModel.objects.filter(cart=cart).exclude(product__id__in=session_product_ids).delete()
 
 
     def save(self):
         self.session.modified = True
+
+
--- core/cart/models.py
+++ core/cart/models.py
@@ -7,6 +7,9 @@ class CartModel(models.Model):
     user = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.email
     
 
 
@@ -17,4 +20,7 @@ class CartItemModel(models.Model):
     
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
-  
+    
+    def __str__(self):
+        return f"{self.product.title},{self.cart.id}"
+    
