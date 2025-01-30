c965165ce05c112afa03315063bd62860a1836b5
FahimReza <FahimReza202@outlook.com>
2025-01-30 13:04:41 +0330

    Create The Crud For UserAddressModel in dashboard customer

--- core/dashboard/customer/urls.py
+++ core/dashboard/customer/urls.py
@@ -9,4 +9,10 @@ urlpatterns = [
     path("security/edit/", views.CustomerSecurityEditView.as_view(), name="security-edit"),
     path("profile/edit/", views.CustomerProfileEditView.as_view(), name="profile-edit"),
     path("profile/image/edit/", views.CustomerProfileImageEditView.as_view(), name="profile-image-edit"),
+
+    # Addresses Url
+    path("address/list/", views.CustomerAddressListView.as_view(), name="address-list"),
+    path("address/<int:pk>/edit/", views.CustomerAddressEditView.as_view(), name="address-edit"),
+    path("address/<int:pk>/delete/", views.CustomerAddressDeleteView.as_view(), name="address-delete"),
+    path("address/create/", views.CustomerAddressCreateView.as_view(), name="address-create"),
 ]
