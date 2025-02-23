1d047cbcfc5ea5fb7b5e09ee1f7f1d46c2f3a594
FahimReza <FahimReza202@outlook.com>
2025-02-23 19:02:27 +0330

    Create The models for review app

--- core/review/models.py
+++ core/review/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from shop.models import ProductModel
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
+
+class ReviewStatusType(models.IntegerChoices):
+    pending = 1, "در انتظار تایید"
+    accepted = 2, "تایید شده"
+    rejected = 3, "رد شده"
+
+class ReviewModel(models.Model):
+    user = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
+    product = models.ForeignKey(ProductModel, on_delete=models.PROTECT)
+    description = models.CharField()
+    rate = models.IntegerField(default=5, validators=[MinValueValidator(0),MaxValueValidator(5)])
+    status = models.IntegerField(choices=ReviewStatusType.choices, default=ReviewStatusType.pending.value)
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
